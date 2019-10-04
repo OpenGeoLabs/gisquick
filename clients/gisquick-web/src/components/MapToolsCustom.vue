033acd51c8db79030ceace0effe8f806c62f1bd7
Marcel Dancak <dancakm@gmail.com>
2019-10-04 22:27:59 +0200

    mapotip: Customized identification tool

---
+++ clients/gisquick-web/src/components/MapToolsCustom.vue
@@ -0,0 +1,32 @@
+<script>
+import Vue from 'vue'
+
+import MapTools from './MapTools.vue'
+import Identification from './Identification'
+
+export default {
+  extends: MapTools,
+  computed: {
+    identification () {
+      return {
+        name: 'identification',
+        props: Vue.observable({
+          identificationLayer: '',
+          displayMode: 'info-panel'
+        }),
+        component: Identification
+      }
+    }
+  },
+  watch: {
+    activeTool: {
+      immediate: true,
+      handler (activeTool) {
+        if (!activeTool) {
+          this.$store.commit('activeTool', 'identification')
+        }
+      }
+    }
+  }
+}
+</script>
